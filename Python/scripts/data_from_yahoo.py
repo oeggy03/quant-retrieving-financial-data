cf26a72c1f4823497b5d733bf21f5c1fd3fc5f5d
oeggy03 <hathu2516@gmail.com>
2024-06-17 17:36:06 +0800

    Edit data_from_yahoo.py to ensure that code to retrieve stock data from yahoo can handle errors. Add unit tests for data_from_yahoo.py in data_from_yahoo_test.py.

--- Python/scripts/data_from_yahoo.py
+++ Python/scripts/data_from_yahoo.py
@@ -1,36 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-#
-
-"""
-In order to run this or almost any other Python script,
-the user needs to be able to load/install 3rd-party Python modules/packages from the internet.
-
-There are many ways to do this; we will propose one way:
-
-Install Python IDE/Debugger called Spyder as a part of a bigger package called Anaconda.
-
-First, download and install Anaconda from https://www.anaconda.com/products/individual
-
-Then launch Anaconda and install Spyder in-app from https://docs.spyder-ide.org/current/installation.html
-
-After that, follow the instructions on https://stackoverflow.com/questions/10729116/adding-a-module-specifically-pymorph-to-spyder-python-ide
-and run the command:
-
-pip install pandas_datareader
-
-If you need to load/install other Python 3rd-party packages, then run:
-pip install "name of package"
-Example:
-pip install numpy
-pip install pandas
-
-This script downloads and prints daily OHLCV data for the given instruments.
-"""
-
-from pandas_datareader import data as pdr
-import yfinance as yf
 import datetime
+import yfinance as yf
+from pandas_datareader import data as pdr
+import unittest
 
 yf.pdr_override()
 
@@ -47,6 +18,13 @@ def fetch_yfinance_data(start_date, end_date, *symbols):
     Returns:
     None
     """
+    # Validate date format
+    try:
+        datetime.datetime.strptime(start_date, '%Y-%m-%d')
+        datetime.datetime.strptime(end_date, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Incorrect date format, should be YYYY-MM-DD")
+
     for symbol in symbols:
         try:
             print(f">> {symbol} ... ", end='')
@@ -60,11 +38,7 @@ if __name__ == "__main__":
     start_date = '2023-09-25'
     end_date = '2023-10-02'
 
-    # Validate date format
     try:
-        datetime.datetime.strptime(start_date, '%Y-%m-%d')
-        datetime.datetime.strptime(end_date, '%Y-%m-%d')
-    except ValueError:
-        print("Incorrect date format, should be YYYY-MM-DD")
-    else:
         fetch_yfinance_data(start_date, end_date, 'MSFT', 'IWO', 'VFINX', 'BTC-USD')
+    except Exception as e:
+        print(e)
---
+++ Python/test/data_from_yahoo_test.py
@@ -0,0 +1,53 @@
+import unittest
+
+from Python.scripts.data_from_yahoo import fetch_yfinance_data
+
+
+class TestFetchYFinanceData(unittest.TestCase):
+
+    def test_valid_symbols_and_dates(self):
+        # Test with valid symbols and valid date range
+        start_date = '2023-09-25'
+        end_date = '2023-10-02'
+        symbols = ['MSFT', 'IWO', 'VFINX', 'BTC-USD']
+        fetch_yfinance_data(start_date, end_date, *symbols)
+
+    def test_invalid_symbols(self):
+        # Test with some invalid symbols
+        start_date = '2023-09-25'
+        end_date = '2023-10-02'
+        symbols = ['INVALID1', 'INVALID2']
+        fetch_yfinance_data(start_date, end_date, *symbols)
+
+    def test_mixed_valid_and_invalid_symbols(self):
+        # Test with a mix of valid and invalid symbols
+        start_date = '2023-09-25'
+        end_date = '2023-10-02'
+        symbols = ['MSFT', 'INVALID']
+        fetch_yfinance_data(start_date, end_date, *symbols)
+
+    def test_empty_symbols(self):
+        # Test with no symbols
+        start_date = '2023-09-25'
+        end_date = '2023-10-02'
+        symbols = []
+        fetch_yfinance_data(start_date, end_date, *symbols)
+
+    def test_invalid_date_format(self):
+        # Test with invalid date format
+        start_date = '2023-25-09'
+        end_date = '2023-02-10'
+        symbols = ['MSFT']
+        with self.assertRaises(ValueError):
+            fetch_yfinance_data(start_date, end_date, *symbols)
+
+    def test_end_date_before_start_date(self):
+        # Test with end date before start date
+        start_date = '2023-10-02'
+        end_date = '2023-09-25'
+        symbols = ['MSFT']
+        fetch_yfinance_data(start_date, end_date, *symbols)
+
+
+if __name__ == "__main__":
+    unittest.main()
