eac349424405fa969d982858ef5029077c2180d4
oeggy03 <hathu2516@gmail.com>
2024-06-17 17:26:02 +0800

    Update read_data_from_yahoo_fin.py to be more general and callable for any given token.

--- Python/read_data_from_yahoo_fin.py
+++ Python/read_data_from_yahoo_fin.py
@@ -2,52 +2,69 @@
 # -*- coding: UTF-8 -*-
 #
 
-
 """
-in order to run this or almost any other Python script
-the user needs to be able to load/install 3rd-party Python modules/packages from the internet
+In order to run this or almost any other Python script,
+the user needs to be able to load/install 3rd-party Python modules/packages from the internet.
 
-There are many ways to do this, we will propose one way:
+There are many ways to do this; we will propose one way:
 
-Install Python IDE/DEbugger calles Spyder as a part of bigger package called Anaconda
+Install Python IDE/Debugger called Spyder as a part of a bigger package called Anaconda.
 
-Firtst download and install Anaconda from   https://www.anaconda.com/products/individual
+First, download and install Anaconda from https://www.anaconda.com/products/individual
 
-and then launch Anaconda and install Spyder in-app from
+Then launch Anaconda and install Spyder in-app from https://docs.spyder-ide.org/current/installation.html
 
-https://docs.spyder-ide.org/current/installation.html
-
-After that following https://stackoverflow.com/questions/10729116/adding-a-module-specifically-pymorph-to-spyder-python-ide
-run command
+After that, follow the instructions on https://stackoverflow.com/questions/10729116/adding-a-module-specifically-pymorph-to-spyder-python-ide
+and run the command:
 
 pip install pandas_datareader
 
-if you need to load/install other Python 3rd-party pankages, then tun
+If you need to load/install other Python 3rd-party packages, then run:
 pip install "name of package"
-example
+Example:
 pip install numpy
 pip install pandas
 
-This scripts downloads and prints daily OHLCV data for the following 4 instruments
-
-- Stock: Microsoft
-- ETF: Russell 2000 Growth
-- Mutual fund: Vanguard 500 Index fund
-- Currency BTC-USD
+This script downloads and prints daily OHLCV data for the given instruments.
 """
 
 from pandas_datareader import data as pdr
 import yfinance as yf
+import datetime
 
 yf.pdr_override()
 
 
-def test_yfinance():
-    for symbol in ['MSFT', 'IWO', 'VFINX', 'BTC-USD']:
-        print(">>", symbol, end=' ... ')
-        data = pdr.get_data_yahoo(symbol, start='2023-09-25', end='2023-10-02')
-        print(data)
+def fetch_yfinance_data(start_date, end_date, *symbols):
+    """
+    Fetches OHLCV data for the given symbols from Yahoo Finance.
+
+    Parameters:
+    start_date (str): Start date in 'YYYY-MM-DD' format.
+    end_date (str): End date in 'YYYY-MM-DD' format.
+    symbols (tuple): Tuple of stock/ETF/mutual fund/currency symbols.
+
+    Returns:
+    None
+    """
+    for symbol in symbols:
+        try:
+            print(f">> {symbol} ... ", end='')
+            data = pdr.get_data_yahoo(symbol, start=start_date, end=end_date)
+            print(data)
+        except Exception as e:
+            print(f"Failed to retrieve data for {symbol}: {e}")
 
 
 if __name__ == "__main__":
-    test_yfinance()
+    start_date = '2023-09-25'
+    end_date = '2023-10-02'
+
+    # Validate date format
+    try:
+        datetime.datetime.strptime(start_date, '%Y-%m-%d')
+        datetime.datetime.strptime(end_date, '%Y-%m-%d')
+    except ValueError:
+        print("Incorrect date format, should be YYYY-MM-DD")
+    else:
+        fetch_yfinance_data(start_date, end_date, 'MSFT', 'IWO', 'VFINX', 'BTC-USD')
